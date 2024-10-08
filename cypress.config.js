1ca84c6e7c0b47fdad9eef08710022ee59e5be85
Caio Ventura <37090076+caiovn@users.noreply.github.com>
2024-10-08 20:17:31 -0300

    setup cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  "cypress-cucumber-preprocessor": {
-    nonGlobalStepDefinitions: false,
-    step_definitions: "./cypress/support/steps",
-  },
   e2e: {
     setupNodeEvents(on, config) {
       return require("./cypress/plugins/index.js")(on, config);
--- cypress/integration/list/1_quiz_list_cards.feature
+++ cypress/integration/list/1_quiz_list_cards.feature
@@ -1,6 +1,5 @@
 # language: pt
-
 Funcionalidade: Listagem dos quizzes
 
-Cenario: Verifica listagem de quizzes
+  Cenario: Verifica listagem de quizzes
     Dado que estou na listagem de quizzes
